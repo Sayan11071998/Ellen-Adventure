1615eb6d337f56da8cffb9950999a3a5c73b338f
Sayan Nandi <sayannandi11071998@gmail.com>
2024-10-15 00:52:49 +0530

    Chapter 3 - Assignment - Vertical input & Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,51 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
-    public float speed;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private BoxCollider2D playerBoxCollider2d;
+    [SerializeField] private Rigidbody2D playerRigidBody2d;
+    [SerializeField] private float playerHorizontalSpeed;
+    [SerializeField] private float playerVerticalJumpHeight;
+
+    private Vector2 boxColInitSize;
+    private Vector2 boxColInitOffset;
+    private bool isGrounded = false;
+
+    private void Start()
+    {
+        boxColInitSize = playerBoxCollider2d.size;
+        boxColInitOffset = playerBoxCollider2d.offset;
+    }
 
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Jump");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Jump");
 
-        MoveCharacter(horizontal);
-        PlayMovementAnimation(horizontal, vertical);
-    }
+        MoveCharacterHorizontally(horizontalInput, verticalInput);
+        PlayMovementAnimation(horizontalInput, verticalInput);
 
-    private void MoveCharacter(float horizontal)
-    {
-        Vector3 position = transform.position;
-        position.x += horizontal * speed * Time.deltaTime;
-        transform.position = position;
-    }
-
-    private void PlayMovementAnimation(float horizontal, float vertical)
-    {
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
-
-        Vector3 scale = transform.localScale;
-        if (horizontal < 0)
+        if (Input.GetKey(KeyCode.LeftControl))
         {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        else if (horizontal > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
-
-        // Jump
-        if (vertical > 0)
-        {
-            animator.SetBool("Jump", true);
+            PlayCrouchAnimation(true);
         }
         else
         {
-            animator.SetBool("Jump", false);
+            PlayCrouchAnimation(false);
         }
+    }
+
+    public void MoveCharacterHorizontally(float horizontalInput, float verticalInput)
+    {
+        // Move Character Horizontally
+        Vector3 position = transform.position;
+        position.x += horizontalInput * playerHorizontalSpeed * Time.deltaTime;
+        transform.position = position;
+
+        // Move Character Vertically
+        if(verticalInput > 0){
+            playerRigidBody2d.AddForce(new Vector2(0f, playerVerticalJumpHeight), ForceMode2D.Force);
+        }
+    }
+
+    public void PlayMovementAnimation(float horizontalInput, float verticalInput)
+    {
+        playerAnimator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        Vector2 localScale = transform.localScale;
+        if (horizontalInput < 0)
+        {
+            localScale.x = -1f * Mathf.Abs(localScale.x);
+        }
+        else if (horizontalInput > 0)
+        {
+            localScale.x = Mathf.Abs(localScale.x);
+        }
+        transform.localScale = localScale;
+
+        // Jump
+        if (verticalInput > 0)
+        {
+            playerAnimator.SetBool("Jump", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Jump", false);
+        }
+    }
+
+    public void PlayCrouchAnimation(bool crouchValue)
+    {
+        if (crouchValue == true)
+        {
+            float sizeValueX = 0.9508577f;
+            float sizeValueY = 1.311901f;
+
+            float offsetValueX = -0.1270343f;
+            float offsetValueY = 0.5678566f;
+
+            playerBoxCollider2d.size = new Vector2(sizeValueX, sizeValueY);
+            playerBoxCollider2d.offset = new Vector2(offsetValueX, offsetValueY);
+        }
+        else
+        {
+            playerBoxCollider2d.size = boxColInitSize;
+            playerBoxCollider2d.offset = boxColInitOffset;
+        }
+
+        playerAnimator.SetBool("Crouch", crouchValue);
     }
 }
